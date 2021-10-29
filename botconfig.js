1115ee37c4269a715e909ae2eb872d04e76a044f
AceKiller250 <74756559+AceKiller250@users.noreply.github.com>
2021-10-28 23:49:21 -0700

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -28,10 +28,10 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "host name or IP",
-    port: 80, // The port that lavalink is listening to. This must be a number!
-    pass: "your password",
-    secure: false, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
+    host: "lavalink-railways2-production-a9c3.up.railway.app",
+    port: 443, // The port that lavalink is listening to. This must be a number!
+    pass: "",
+    secure: true, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
   },
 
   //Please go to https://developer.spotify.com/dashboard/
