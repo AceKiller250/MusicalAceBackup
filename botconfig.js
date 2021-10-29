cd90ab0d7d11486e1f4ed9136cc750d598efd60c
AceKiller250 <74756559+AceKiller250@users.noreply.github.com>
2021-10-29 00:05:51 -0700

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -28,8 +28,9 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "lavalink-railways2-production-a9c3.up.railway.app",
+    host: "lavalink-repl-1.acekiller250.repl.co",
     port: 443, // The port that lavalink is listening to. This must be a number!
+    pass: "youshallnotpass",
     secure: true, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
   },
 
