8b1da13312ff5f7e4e663b8d6078d4f5e502e192
AceKiller250 <74756559+AceKiller250@users.noreply.github.com>
2021-11-01 22:57:42 -0700

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -28,7 +28,7 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "lavalink-repl-1.acekiller250.repl.co",
+    host: "https://lavalinkforbot250.herokuapp.com",
     port: 443, // The port that lavalink is listening to. This must be a number!
     pass: "youshallnotpass",
     secure: true, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
